6711d11d4c8dc8db4bf35437599b791768cf4196
NezT1R <ryzhenkowork@gmail.com>
2025-04-09 22:07:32 +0300

    delete second branch

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ function App() {
 
   return (
     <>
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam veniam a vel debitis eius, illo error inventore nisi. Doloremque quae sunt optio aspernatur sint tenetur nemo fugit omnis laborum soluta.
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam veniam a vel debitis eius, illo error inventore nisi. Doloremque quae sunt optio aspernatur sint tenetur nemo fugit omnis laborum soluta. Totam veniam a vel debitis eius, illo error inventore nisi. Doloremque quae sunt optio aspernatur sint tenetur nemo fugit omnis laborum soluta.
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam veniam a vel debitis eius, illo error inventore nisi. Doloremque quae sunt optio aspernatur sint tenetur nemo fugit omnis laborum soluta.
